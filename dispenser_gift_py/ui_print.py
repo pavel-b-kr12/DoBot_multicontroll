8f1fa436aad6ca409f395bca5fb5821c8cd74f41
kr12 <kr16@ukr.net>
2020-02-20 17:21:06 +0200

    .

---
+++ dispenser_gift_py/ui_print.py
@@ -0,0 +1,17 @@
+
+from __future__ import print_function
+#import fixpath
+from colorama import init as initcolorama, Fore, Back, Style
+initcolorama()
+
+
+def printColorizeStr(s,b, str_b=["",""]):
+	if(b):
+		return Fore.GREEN+s+str_b[1]+Fore.RESET
+	else:
+		return Fore.RED+s+str_b[0]+Fore.RESET
+def printColorizeStr(s,b, str_b, color_b=[Fore.RED,Fore.GREEN]):
+	if(b):
+		return color_b[1]+s+str_b[1]+Fore.RESET
+	else:
+		return color_b[0]+s+str_b[0]+Fore.RESET
